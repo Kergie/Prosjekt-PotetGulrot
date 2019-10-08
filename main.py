e53c1006828835d2e2310b5fa422a2ef0e52b071
kergie <h_waldum@hotmail.com>
2019-10-08 11:35:01 +0200

    added basic code for reading webcamera and showing the frames, as well as quitting the showing of frames.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import numpy
+import numpy as np
+import cv2
+
+import time
+
+cap = cv2.VideoCapture(0)
+time.sleep(0.01)
+
+while(True):
+    ret, frame = cap.read()
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
