768969ad0f6fb7eb50c76304fbf992770abbf055
kergie <h_waldum@hotmail.com>
2019-10-09 10:08:07 +0200

    Finetuned edge detection, added conversion to cm from pixels in test on screen.

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import cv2
 
 import time
 
+snusBoksConstantArea = 38.48  # cm2
+snusBoksConstantPerim = 21.99 # cm
+pixelPerMetric = 0
 
 cap = cv2.VideoCapture(0)
 time.sleep(0.01)
+
+def putTextInFrame(frame, xcord, ycord, variableType:str, variable):
+    cv2.putText(frame, variableType + str('%.2f' % round(variable, 2)) + 'cm', (xcord, ycord), color=(255,0,0),
+                thickness=2, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5)
 
 while(True):
     _, frame = cap.read()
@@ -13,11 +20,11 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-    edges = cv2.Canny(gray, 100, 200)
+    edges = cv2.Canny(gray, 100, 180, apertureSize=3)
     edges = cv2.dilate(edges, None, iterations=1)
     edges = cv2.erode(edges, None, iterations=1)
 
-    cnts, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
 
@@ -26,15 +33,19 @@ while(True):
     if len(cnts) > 0:
         for cnt in cnts:
             area = cv2.contourArea(cnt)
-            if area > 100:
+            if area > 1000:
                 cnts_filt.append(cnt)
         if len(cnts_filt) > 0:
-            cv2.putText(frame, 'Omkrets: ' + str(cv2.arcLength(cnts_filt[0], True)), (50,50), color=(255,0,0), thickness=3,
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1)
-            cv2.putText(frame, 'Areal: ' + str(cv2.contourArea(cnts_filt[0])), (50, 100), color=(255, 0, 0),
-                        thickness=3,
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1)
+            pixelPerMetricArea = snusBoksConstantArea / cv2.contourArea(cnts_filt[0])
+            pixelPerMetricPerim = snusBoksConstantPerim / cv2.arcLength(cnts_filt[0], True)
+            putTextInFrame(frame, 50, 50, 'Omkrets: ', cv2.arcLength(cnts_filt[0], True)*pixelPerMetricPerim)
+            putTextInFrame(frame, 50, 100, 'Areal: ', cv2.contourArea(cnts_filt[0])*pixelPerMetricArea)
+            if len(cnts_filt) > 1:
+                putTextInFrame(frame, 300, 50, 'Omkrets: ', cv2.arcLength(cnts_filt[1], True)*pixelPerMetricPerim)
+                putTextInFrame(frame, 300, 100, 'Areal: ', cv2.contourArea(cnts_filt[1])*pixelPerMetricArea)
+                cv2.fillPoly(frame, pts=[cnts_filt[1]], color=(0,255,0))
             cv2.fillPoly(frame, pts=[cnts_filt[0]], color=(255,0,0))
+
             cv2.drawContours(frame, cnts_filt, -1, (0, 255, 0), 3)
 
     cv2.imshow("Frame", frame)
